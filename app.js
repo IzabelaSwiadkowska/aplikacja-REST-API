3e3cfab6ba894e5afc00f14ed7636a575c19eec1
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-10-08 18:41:02 +0200

    hmw05

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const contactsRouter = require('./contacts/contacts.router');
 const usersRouter = require('./users/users.router');
@@ -8,10 +9,11 @@ const usersRouter = require('./users/users.router');
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
-
+app.use(express.static('public'));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/contacts', contactsRouter);
 app.use('/users', usersRouter);
---
+++ users/users.test.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose');
+const app = require('../app');
+const supertest = require('supertest');
+const { uriDb } = require('../config');
+require('dotenv').config();
+
+beforeAll(async () => {
+  await mongoose.connect(uriDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+mongoose.set('strictQuery', false);
+
+describe('singup controller', () => {
+  const credentials = {
+    email: 'izabel@gmail.com',
+    password: 'h!741ds',
+  };
+
+  test('Should create user and return him', async () => {
+    const resSignup = await supertest(app)
+      .post('/users/signup')
+      .send(credentials);
+    expect(resSignup.statusCode).toBe(201);
+    expect(resSignup.body).toBeTruthy();
+    expect(typeof resSignup.body.user).toBe('object');
+    expect(typeof resSignup.body.user.email).toBe('string');
+    expect(typeof resSignup.body.user.subscription).toBe('string');
+  });
+});
+
+describe('login controller', () => {
+  const credentials = {
+    email: 'izabel@gmail.com',
+    password: 'h!741ds',
+  };
+  test('Should  login a user and return status code 200', async () => {
+    const resLogin = await supertest(app)
+      .post('/users/login')
+      .send(credentials);
+    expect(resLogin.statusCode).toBe(200);
+    expect(resLogin.body.token).toBeTruthy();
+    expect(typeof resLogin.body.user).toBe('object');
+    expect(typeof resLogin.body.user.email).toBe('string');
+    expect(typeof resLogin.body.user.subscription).toBe('string');
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
