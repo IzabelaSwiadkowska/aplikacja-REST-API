f944f9e3a2f843e11dfd5c4416dbde36dde1a81c
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-10-09 05:42:43 +0200

    trycatch added

--- users/users.test.js
+++ users/users.test.js
@@ -6,10 +6,14 @@ const { uriDb } = require('../config');
 require('dotenv').config();
 
 beforeAll(async () => {
-  await mongoose.connect(uriDb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    await mongoose.connect(uriDb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 mongoose.set('strictQuery', false);
@@ -21,14 +25,18 @@ describe('singup controller', () => {
   };
 
   test('Should create a  user and return him', async () => {
-    const resSignup = await supertest(app)
-      .post('/users/signup')
-      .send(credentials);
-    expect(resSignup.statusCode).toBe(201);
-    expect(resSignup.body).toBeTruthy();
-    expect(typeof resSignup.body.user).toBe('object');
-    expect(typeof resSignup.body.user.email).toBe('string');
-    expect(typeof resSignup.body.user.subscription).toBe('string');
+    try {
+      const resSignup = await supertest(app)
+        .post('/users/signup')
+        .send(credentials);
+      expect(resSignup.statusCode).toBe(201);
+      expect(resSignup.body).toBeTruthy();
+      expect(typeof resSignup.body.user).toBe('object');
+      expect(typeof resSignup.body.user.email).toBe('string');
+      expect(typeof resSignup.body.user.subscription).toBe('string');
+    } catch (e) {
+      console.error(e);
+    }
   });
 });
 
@@ -38,17 +46,25 @@ describe('login controller', () => {
     password: 'h!741ds',
   };
   test('Should login a user and return status code 200', async () => {
-    const resLogin = await supertest(app)
-      .post('/users/login')
-      .send(credentials);
-    expect(resLogin.statusCode).toBe(200);
-    expect(resLogin.body.token).toBeTruthy();
-    expect(typeof resLogin.body.user).toBe('object');
-    expect(typeof resLogin.body.user.email).toBe('string');
-    expect(typeof resLogin.body.user.subscription).toBe('string');
+    try {
+      const resLogin = await supertest(app)
+        .post('/users/login')
+        .send(credentials);
+      expect(resLogin.statusCode).toBe(200);
+      expect(resLogin.body.token).toBeTruthy();
+      expect(typeof resLogin.body.user).toBe('object');
+      expect(typeof resLogin.body.user.email).toBe('string');
+      expect(typeof resLogin.body.user.subscription).toBe('string');
+    } catch (e) {
+      console.error(e);
+    }
   });
 
   afterAll(async () => {
-    await mongoose.connection.close();
+    try {
+      await mongoose.connection.close();
+    } catch (e) {
+      console.error(e);
+    }
   });
 });
